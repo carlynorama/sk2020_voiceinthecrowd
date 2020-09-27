2ffee8d651689c7d328ed28a6837eb693eacfcaf
carlynorama <carlynorama@gmail.com>
2020-09-27 12:08:42 -0700

    folicle data load

--- HardwareHandler.h
+++ HardwareHandler.h
@@ -2,32 +2,62 @@
 const int nose = 0;  //A0 input
 
 const int pushButton = 4;
-bool buttonState = HIGH;
+const bool trueState = LOW;
+bool buttonState = false;
 
+const int folicleCount = 4;
 const int folicle1 = 7;  //A0 input
 const int folicle2 = 6;  //A0 input
 const int folicle3 = 5;  //A0 input
 const int folicle4 = 4;  //A0 input
 
-int folicles[4] = { folicle1, folicle2, folicle3, folicle4 };
+int folicles[folicleCount] = { folicle1, folicle2, folicle3, folicle4 };
+int folicleReadings[folicleCount] = { 0, 0, 0, 0 };
 
 //20*5*number
 //20Hz - 655Hz
 
 void checkButton() {
-  buttonState = digitalRead(pushButton);
+  if (trueState == digitalRead(pushButton)) {
+    buttonState = true;
+  } else {
+    buttonState = false;
+  }
 }
 
-int readFolicle(int folicle)
-{
+
+int readFolicle(int folicle) {
+  //  if (Serial) {
+  //    Serial.print("readFolicle ");
+  //    Serial.print(folicle);
+  //    Serial.print(" result: ");
+  //  }
+
   int val = analogRead(folicle);
   uint8_t reading = (uint8_t) (map(val, 0, 1023, 0, 127));
 //  if (Serial) {
-//    Serial.print("readFolicle result: ");
 //    Serial.println(reading);
 //  }
   return reading;
 }
+
+void updateFolicleData() {
+  for (int i = 0; i < folicleCount; i++) {
+    folicleReadings[i] = readFolicle(folicles[i]);
+  }
+}
+
+void printFolicleData() {
+  for (int i = 0; i < folicleCount; i++) {
+    if (Serial) {
+      Serial.print("readFolicle ");
+      Serial.print(i);
+      Serial.print(" result: ");
+      Serial.println(folicleReadings[i]);
+    }
+  }
+}
+
 
 //String getValueForMessage() {
 //  int val = analogRead(nose);
--- SK2020_MusicSender.ino
+++ SK2020_MusicSender.ino
@@ -114,16 +114,11 @@ void loop() {
   checkButton();
 
   //-------------------------------------  Read Analog Hardware
+  updateFolicleData();
 
   //-------------------------------------  Update Local World
     if (buttonState == true) {
-
-      int newFolicleReading = readFolicle(folicle1);
-      Serial.print("Loop reading 1: ");
-      Serial.println(newFolicleReading);
-      newFolicleReading = readFolicle(folicle2);
-      Serial.print("Loop reading 2: ");
-      Serial.println(newFolicleReading);
+      printFolicleData();
     }
 
   //-------------------------------------  Update Remote World
